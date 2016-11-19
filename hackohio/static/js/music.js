36e25bbc889a92cd6a09c445c2a550b2d47baef0
Wes Weber <wweber@wesweber.com>
2016-11-19 16:19:49 -0500

    Add audio analyser base code.

--- hackohio/static/js/music.js
+++ hackohio/static/js/music.js
@@ -4,6 +4,7 @@ window.music = new function () {
     var This = this;
 
     this.currentPlaylist = [];
+    this.analyser = null;
 
     this.playToggle = function(element) {
         if ($(element).hasClass("glyphicon-play")) {
@@ -60,6 +61,51 @@ window.music = new function () {
         $.get("/playlist/" + mood, null, function(data, status, xhr) {
             callback(data);
         });
+    };
+
+    var renderAnalyser = function() {
+        var freqData = new Uint8Array(This.analyser.frequencyBinCount);
+        This.analyser.getByteFrequencyData(freqData);
+
+        var avgFreqData = new Float32Array(50);
+
+        for (var i = 0; i < 50; i++) {
+            var minIdx = i * Math.floor(freqData.length / avgFreqData.length);
+            var maxIdx = minIdx + Math.floor(freqData.length /
+                    avgFreqData.length) - 1;
+
+            if (maxIdx >= freqData.length)
+                maxIdx = freqData.length - 1;
+
+            var count = maxIdx - minIdx + 1;
+
+            avgFreqData[i] = 0;
+
+            for (j = minIdx; j <= maxIdx; j++) {
+                avgFreqData[i] += freqData[j];
+            }
+
+            avgFreqData[i] /= count;
+        }
+
+        console.log(avgFreqData);
+    };
+
+    this.attachAnalyser = function(element) {
+        var ctx = new AudioContext();
+        var src = ctx.createMediaElementSource(element);
+        var analyser = ctx.createAnalyser();
+
+        This.analyser = analyser;
+
+        console.log(analyser.fftSize);
+        analyser.fftSize = 2048;
+
+        src.connect(analyser);
+
+        window.setInterval(function (ev) {
+            renderAnalyser();
+        }, 100);
     };
 
     document.addEventListener("click", function(ev) {
