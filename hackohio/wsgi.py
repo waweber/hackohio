fc622669b43fc2dc320566025ab75bb561d42d92
Wes Weber <wweber@wesweber.com>
2016-11-19 13:27:11 -0500

    Add playlist endpoint.

--- hackohio/wsgi.py
+++ hackohio/wsgi.py
@@ -17,6 +17,7 @@ def main(global_config, **settings):
 
     # Routes
     config.add_route("index", "/")
+    config.add_route("playlist", "/playlist/{name}")
 
     logger.info("Creating WSGI server")
     config.scan()
@@ -26,3 +27,69 @@ def main(global_config, **settings):
 @view_config(route_name="index", renderer="index.html", request_method="GET")
 def index_view(request):
     return {}
+
+
+@view_config(route_name="playlist", renderer="json", request_method="GET")
+def playlist_view(request):
+    name = request.matchdict.get("name")
+
+    if name == "happy":
+        return [{
+            "media": "/static/media/happy1.ogg",
+            "cover": "/static/media/happy1.jpg",
+            "title": "Happy Song 1",
+            "artist": "Happy Guy",
+            "album": "Happy Album",
+        }, {
+            "media": "/static/media/happy2.ogg",
+            "cover": "/static/media/happy2.jpg",
+            "title": "Happy Song 2",
+            "artist": "Happy Guy",
+            "album": "Happy Album 2",
+        }, {
+            "media": "/static/media/happy3.ogg",
+            "cover": "/static/media/happy3.jpg",
+            "title": "Happy Song 3",
+            "artist": "Happy Person",
+            "album": "Happy II",
+        }]
+    elif name == "sad":
+        return [{
+            "media": "/static/media/sad1.ogg",
+            "cover": "/static/media/sad1.jpg",
+            "title": "Sad Song 1",
+            "artist": "Sad Guy",
+            "album": "Sad Album",
+        }, {
+            "media": "/static/media/sad2.ogg",
+            "cover": "/static/media/sad2.jpg",
+            "title": "Sad Song 2",
+            "artist": "Sad Guy",
+            "album": "Sad Album 2",
+        }, {
+            "media": "/static/media/sad3.ogg",
+            "cover": "/static/media/sad3.jpg",
+            "title": "Sad Song 3",
+            "artist": "Sad Person",
+            "album": "Sad II",
+        }]
+    else:
+        return [{
+            "media": "/static/media/normal1.ogg",
+            "cover": "/static/media/normal1.jpg",
+            "title": "Normal Song 1",
+            "artist": "Normal Guy",
+            "album": "Normal Album",
+        }, {
+            "media": "/static/media/normal2.ogg",
+            "cover": "/static/media/normal2.jpg",
+            "title": "Normal Song 2",
+            "artist": "Normal Guy",
+            "album": "Normal Album 2",
+        }, {
+            "media": "/static/media/normal3.ogg",
+            "cover": "/static/media/normal3.jpg",
+            "title": "Normal Song 3",
+            "artist": "Normal Person",
+            "album": "Normal II",
+        }]
