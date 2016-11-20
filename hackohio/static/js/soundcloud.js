03041736346be41b75e9435a26270b2e557f37e4
Wes Weber <wweber@wesweber.com>
2016-11-19 22:19:40 -0500

    Work on soundcloud.

---
+++ hackohio/static/js/soundcloud.js
@@ -0,0 +1,10 @@
+window.soundcloud = new function() {
+
+    var This = this;
+
+    this.getTracks = function(callback) {
+    };
+
+    window.addEventListener("load", function(ev) {
+    });
+}();
