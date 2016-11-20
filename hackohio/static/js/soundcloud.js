4aecdd9ea42ca6d23fab5735d3270321aac23f75
Wes Weber <wweber@wesweber.com>
2016-11-20 03:09:02 -0500

    Update soundcloud.

--- hackohio/static/js/soundcloud.js
+++ hackohio/static/js/soundcloud.js
@@ -2,6 +2,8 @@ window.soundcloud = new function() {
 
     var This = this;
 
+    
+
     this.getTracks = function(playlist_id, callback) {
         var url = "/soundcloud/tracks"
 
@@ -10,17 +12,22 @@ window.soundcloud = new function() {
         };
 
         $.get(url, params, function(data, status, xhr) {
-            callback(data.tracks);
+            callback(data);
         });
 
     };
 
     this.trackToPlaylistItem = function(track) {
+
+        var cover = track.artwork_url || track.user.avatar_url;
+
+        cover = cover.replace("large", "t500x500")
+
         return {
             artist: track.user.username,
             album: "",
             title: track.title,
-            cover: track.artwork_url || track.user.avatar_url,
+            cover: cover,
             track_id: track.id
         }
     };
@@ -29,8 +36,7 @@ window.soundcloud = new function() {
         var playlist = [];
 
         for (var i in tracks) {
-            if (tracks[i]["user"])
-                playlist.push(This.trackToPlaylistItem(tracks[i]));
+            playlist.push(This.trackToPlaylistItem(tracks[i]));
         }
 
         return playlist;
@@ -41,10 +47,28 @@ window.soundcloud = new function() {
         playCallback(url);
     };
 
-    window.addEventListener("load", function(ev) {
-        This.getTracks(271195793, function(tracks) {
-            var playlist = This.buildPlaylist(tracks);
-            window.music.queuePlaylist(playlist);
+    this.queuePlaylist = function(mood) {
+        var id;
+        if (mood == "happy") {
+            id = 276991253;
+        } else if (mood == "sad") {
+            id = 276989807;
+        } else if (mood == "angry") {
+            id = 276990583;
+        } else {
+            id = 276992887;
+        }
+
+
+        This.getTracks(id, function(tracks) {
+            var pls = This.buildPlaylist(tracks);
+            window.music.queuePlaylist(pls);
+            console.log(tracks);
         });
+
+    };
+
+    window.addEventListener("load", function(ev) {
+        This.queuePlaylist("neutral");
     });
 }();
